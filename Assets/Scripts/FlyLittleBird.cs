6f839170a75af2388cad5864465063595e6f07a4
Michael Kugler <mrkugler@pitt.edu>
2020-02-01 16:48:49 -0500

    First Push

---
+++ Assets/Scripts/FlyLittleBird.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyLittleBird : MonoBehaviour {
+
+    public GameManager gameManager;
+    public float velocity = 1;
+    private Rigidbody rb;
+
+    public AudioSource flySound;
+    public AudioSource dieSound;
+    public AudioSource hitSound;
+
+    public GameObject Title;
+    public GameObject PlayButton;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            //Jump
+            rb.velocity = Vector2.up * velocity;
+            flySound.Play();
+        }
+	}
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        gameManager.GameOver();
+        PlayButton.SetActive(true);
+        Title.SetActive(true);
+        hitSound.Play();
+        dieSound.Play();
+    }
+
+
+}
